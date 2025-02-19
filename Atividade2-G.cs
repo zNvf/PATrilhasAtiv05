1f9a11056499bd676dd7c2be3128406869b0af19
zNvf <tradelost2@gmail.com>
2025-02-19 10:49:20 -0300

    Create Atividade2-G.cs

---
+++ Atividade2-G.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+public static void Main()
+{
+int contador = 0;
+Console.WriteLine("Digite um número: ");
+var numero = int.Parse(Console.ReadLine());
+Console.WriteLine("\n\rOs números pares entre 0 e {0} são: " ,numero);
+do {
+contador++;
+if (contador % 2 == 0 && contador != numero)
+{
+Console.Write(" " +contador);
+}
+} while (contador < numero );
+Console.WriteLine( );
+Console.WriteLine("\n\rPress any key to continue . . . ");
+Console.ReadKey(true);
+}
+}
