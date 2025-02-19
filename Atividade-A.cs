bf06c7971f8385a5067ff9ea2d8227dc58f9eed7
zNvf <tradelost2@gmail.com>
2025-02-19 10:03:55 -0300

    Create Atividade-A.cs

---
+++ Atividade-A.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+public static void Main(string[] args)
+{
+var date = DateTime.Now;
+Console.WriteLine(date);
+
+Console.Write("\n\rPress any key to continue . . . ");
+Console.ReadKey(true);
+}
+}
